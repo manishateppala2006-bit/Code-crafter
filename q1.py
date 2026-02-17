0313cda3cc0a824d277634ca2e29727ac6ea1ad8
manishateppala2006-bit <manishateppala2006@gmail.com>
2026-02-17 19:09:53 +0530

    Add function to calculate total bill with tip

---
+++ q1.py
@@ -0,0 +1,5 @@
+def calculate_total_bill(amount: float, tip_percent: int) -> float:
+    amount = float(amount)
+    tip_percent = float(tip_percent)
+    total = amount + (amount * tip_percent / 100)
+    return round(total, 2)
